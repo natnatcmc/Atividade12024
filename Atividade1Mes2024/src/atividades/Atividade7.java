6a0b564663194a43b20ec0168dbc6d630dec4f1c
Natália Costa Moura <125416283+natnatcmc@users.noreply.github.com>
2024-02-22 20:36:59 -0300

    Add files via upload

---
+++ Atividade1Mes2024/src/atividades/Atividade7.java
@@ -0,0 +1,21 @@
+package atividades;
+
+import java.util.Scanner;
+
+public class Atividade7 {
+	
+    public static void main (String[]args) {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Digite a base do retângulo:");
+	int base= sc.nextInt();
+	
+	System.out.println("Digite a altura do retângulo:");
+	int altura = sc.nextInt();
+	
+	int resultado = base * altura;
+	System.out.println("A área do retângulo:" + resultado);
+    }
+}
+
